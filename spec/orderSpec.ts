e4a4c45f7bce010b590abdb9230b07c93fdacd5a
Damon Clark <dclark312@gmail.com>
2022-06-04 14:45:10 -0400

    add test that is throwing error

--- spec/orderSpec.ts
+++ spec/orderSpec.ts
@@ -1,13 +1,15 @@
 import { OrderList } from '../src/models/order';
 import app from '../src/server'; 
 import supertest from 'supertest';
+import token from '../src/handlers/orders'
 
 const order = new OrderList()
 const request = supertest(app)
 
 describe("Order Model", () => {
-  it('should respond with 200 when passing show /orders/1', async done => {
-    const response = await request.get('/orders/1')
+  fit('should respond with 200 when passing show /orders/1', async done => {
+    console.log(token.toString())
+    const response = await request.get('/orders/1').set('authorization', 'Bearer ' + token); 
     expect(response.statusCode).toBe(200);
     done()
   });
